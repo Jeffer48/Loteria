8a7de1468ef4a4d26eb45c2fdcbc5c6e0456306a
cristian <cristian_enano_99@hotmail.com>
2020-11-29 17:03:56 -0600

    Plantilla y modificacion del html y css

--- js/JuegoLoteria.js
+++ js/JuegoLoteria.js
@@ -1,12 +1,13 @@
 (function() {
     "use strict";
     const cards = document.querySelectorAll(".loteria-card");
-    let firstCard, secondCard;
+
+    /*let firstCard, secondCard;
     let hasFlippedCard = false;
     let lockBoard = false;
     let match = 0;
 
-    function flipCard() {
+    /*function flipCard() {
 
         if (lockBoard) return;
         if (this === firstCard) return;
@@ -20,9 +21,9 @@
         secondCard = this;
         checkForMatch();
         FindelJuego();
-    }
+    }*/
 
-    /* funcion para generar las cartas aleatoriamente */
+    //funcion para generar las cartas aleatoriamente 
     (function Agregarcartas() {
         let index = 0; //sirve para recorrer los div de la pagina JuegoLoteria
         //en este arreglo guarda los numeros de la carta
@@ -34,7 +35,6 @@
             //agregamos al primero en la fila
             if (numerocartas[0] == "") {
                 numerocartas[0] = randomCard;
-
             } /*si ya se introdujo el primero realizamos lo siguiente */
             else {
                 //verificamos los numero para que no se repitan las cartas
@@ -55,15 +55,16 @@
                 }
             }
 
+
             //ponemos la etiqueta imagen con la carta de loteria en una varible con el numero random que nos dio
             let ventana = `<img src="img/Cartas/Loteria-${numerocartas[index - 1]}.png" alt="Carta de loteria" class="front-card"></img>`;
             //sacamos el div con el id para agregar la etiqueta imagen el index nos indica el numero de div
             document.getElementById("posicion" + index).innerHTML = ventana;
         });
-        console.log(numerocartas);
+
     })();
 
-    function FindelJuego() {
+    /*function FindelJuego() {
         setTimeout(() => {
             if (match == 6)
                 alert("Has ganado")
@@ -71,39 +72,39 @@
     }
 
     /* Funcion que revisa la igualdad de las cartas */
-    function checkForMatch() {
-        var puntaje = document.getElementById("puntaje");
-        if (firstCard.dataset.carta === secondCard.dataset.carta) {
-            disableCards();
-            match = 1 + match;
-            puntaje.innerHTML = match * 100;
-        } else {
-            unflipCards();
-        }
-    }
+    /* function checkForMatch() {
+         var puntaje = document.getElementById("puntaje");
+         if (firstCard.dataset.carta === secondCard.dataset.carta) {
+             disableCards();
+             match = 1 + match;
+             puntaje.innerHTML = match * 100;
+         } else {
+             unflipCards();
+         }
+     }
 
-    function unflipCards() {
-        lockBoard = true;
-        setTimeout(() => {
-            firstCard.classList.remove("flip");
-            secondCard.classList.remove("flip");
-            lockBoard = false;
-            resetBoard();
-        }, 1500);
-    }
+     function unflipCards() {
+         lockBoard = true;
+         setTimeout(() => {
+             firstCard.classList.remove("flip");
+             secondCard.classList.remove("flip");
+             lockBoard = false;
+             resetBoard();
+         }, 1500);
+     }
 
-    function disableCards() {
-        firstCard.removeEventListener("click", flipCard);
-        secondCard.removeEventListener("click", flipCard);
-        resetBoard();
-    }
+     function disableCards() {
+         firstCard.removeEventListener("click", flipCard);
+         secondCard.removeEventListener("click", flipCard);
+         resetBoard();
+     }
 
-    function resetBoard() {
-        [hasFlippedCard, lockBoard] = [false, false];
-        [firstCard, secondCard] = [null, null];
-    }
+     function resetBoard() {
+         [hasFlippedCard, lockBoard] = [false, false];
+         [firstCard, secondCard] = [null, null];
+     }
 
-    cards.forEach(card => card.addEventListener("click", flipCard));
+     cards.forEach(card => card.addEventListener("click", flipCard));*/
 
 
 
