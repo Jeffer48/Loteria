03076a6824e48a9c0a8da4d3f6bc23f2b0ced3de
Jeffer48 <60786010+Jeffer48@users.noreply.github.com>
2021-01-23 19:07:05 -0600

    Sistema de amigos

--- perfil.php
+++ perfil.php
@@ -16,6 +16,7 @@
     $amigos = array();
     $objetos = array();
     $amigosIP = array();
+    $amigoName = array();
     $partidasJugadas = 0;
     $partidasGanadas = 0;
     $partidasPerdidas = 0;
@@ -25,7 +26,7 @@
     $monedas = $respuesta[0];
     $imagenPerfil = $respuesta[1];
 
-    $sql = "SELECT amigo FROM amigos WHERE idUsuario = '$id'";
+    $sql = "SELECT amigo FROM amigos WHERE idUsuario = '$id' AND estado = 1";
     $respuesta = solicitarDatos($sql);
 
     while($fila = mysqli_fetch_array($respuesta)){
@@ -53,12 +54,21 @@
     if(count($amigos) > 0){
         for($i = 0; $i < count($amigos); $i++){
             $indice = $amigos[$i];
-            $sql = "SELECT imagenPerfil FROM usuario WHERE idUsuario = $indice";
+            $sql = "SELECT imagenPerfil, nombre FROM usuario WHERE idUsuario = $indice";
             $respuesta = solicitarDatos($sql);
 
             $temp = mysqli_fetch_array($respuesta);
-            array_push($amigosIP, $temp);
+            array_push($amigosIP, $temp[0]);
+            array_push($amigoName, $temp[1]);
         }
+    }
+
+    $pendientes = array();
+    $sql = "SELECT idUsuario FROM amigos WHERE amigo = $id AND estado = 0";
+    $respuesta = solicitarDatos($sql);
+
+    while($fila = mysqli_fetch_array($respuesta)){
+        array_push($pendientes, $fila[0]);
     }
 ?>
 
@@ -92,20 +102,25 @@
                     <button class="btn btn-outline-secondary" type="submit" name="submit" id="button-addon1">Agregar</button>
                     <input type="text" name="amigoID" class="form-control" placeholder="Escriba el identificador" aria-label="Example text with button addon" aria-describedby="button-addon1">
                 </div></form>
-                    <ul class="amigos">
-                        <li><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Amigos</button></li>
-                        <li><button type="button" class="btn btn-warning">Agregar amigo</button></li>
-                        <?php if(count($amigos) > 0){
-                            $c = 0;
-                            if(count($amigos) > 4) $c = 4;
-                            else $c = count($amigos);
-                            for($i = 0; $i < $c; $c++){
-                                $img = $amigosIP[$i];
-                                echo '<li><img class="imgAmi" src="data:image/png;base64,' .  base64_encode($img)  . '" alt="Imagen de perfil"></li>';
-                            }
-                        } 
-                        ?>
-                    </ul>
+                    <form action="perfil.php" method="POST">
+                        <ul class="amigos">
+                            <li><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#pendientes">Pendientes</button></li>
+                            <li><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Amigos</button></li>
+                            <?php if(count($amigos) > 0){
+                                $c = 0;
+                                if(count($amigos) > 3) $c = 3;
+                                else $c = count($amigos);
+                                for($i = 0; $i < $c; $i++){
+                                    $img = $amigosIP[$i];
+                                    echo '<li><img class="listAmi" src="data:image/png;base64,' .  base64_encode($img)  . '" alt="Imagen de perfil">
+                                        <button type="submit" for="nombre" name="verPerfil" class="verPerfiles">'.$amigoName[$i].'</label>
+                                        <input type="text" value="'.$amigoName[$i].'" name="nombreamigo" style="visibility: collapse;">
+                                        <input type="text" value="'.$amigos[$i].'" name="idamigo" style="visibility: collapse;"></li>';
+                                }
+                            } 
+                            ?>
+                        </ul>
+                    </form>
                 </div>
                 <div class="row">
                     <ul class="estadisticas">
@@ -133,7 +148,7 @@
             </div>
             <div>
                 <h2>Objetos comprados</h2>
-                <ul>
+                <ul class="objetos">
                     <?php 
                         $imgObj = array();
                         if(count($objetos) > 0){
@@ -149,9 +164,9 @@
                             $c = 0;
                             if(count($objetos) > 4) $c = 4;
                             else $c = count($objetos);
-                            for($i = 0; $i < $c; $c++){
+                            for($i = 0; $i < $c; $i++){
                                 $imgTemp = $imgObj[$i];
-                                echo '<li><img class="imgOb" src="data:image/png;base64,' .  base64_encode($imgTemp)  . '" alt="Imagen de objeto"></li>';
+                                echo '<li><img class="imgObj" src="data:image/png;base64,' .  base64_encode($imgTemp)  . '" alt="Imagen de objeto"></li>';
                             }
                         }
                     ?>
@@ -167,15 +182,20 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
+                <form action="perfil.php" method="POST">
                     <ul class="amigos">
                         <?php if(count($amigos) > 0){
-                            for($i = 0; $i < count($amigos); $c++){
+                            for($i = 0; $i < count($amigos); $i++){
                                 $img = $amigosIP[$i];
-                                echo '<li><img class="imgAmi" src="data:image/png;base64,' .  base64_encode($img)  . '" alt="Imagen de perfil"></li>';
+                                echo '<li><img class="modalAmi" src="data:image/png;base64,' .  base64_encode($img)  . '" alt="Imagen de perfil">
+                                        <button type="submit" for="nombre" name="verPerfil" class="verPerfiles">'.$amigoName[$i].'</label>
+                                        <input type="text" value="'.$amigoName[$i].'" name="nombreamigo" style="visibility: collapse;">
+                                        <input type="text" value="'.$amigos[$i].'" name="idamigo" style="visibility: collapse;"></li>';
                             }
                         } 
                         ?>
                     </ul>
+                </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
@@ -192,9 +212,9 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                    <ul class="amigos">
+                    <ul class="objetos">
                         <?php if(count($amigos) > 0){
-                            for($i = 0; $i < count($objetos); $c++){
+                            for($i = 0; $i < count($objetos); $i++){
                                 $imgTemp = $imgObj[$i];
                                 echo '<li><img class="imgObj" src="data:image/png;base64,' .  base64_encode($imgTemp)  . '" alt="Imagen de objeto"></li>';
                             }
@@ -208,8 +228,51 @@
             </div>
         </div>
         </div>
+
+        <div class="modal fade" id="pendientes" tabindex="-1" aria-labelledby="pendientesLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="pendientesLabel">Solicitudes pendientes</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <ul class="solicitudes">
+                    <?php
+                        if(count($pendientes) > 0){
+                            for($i = 0; $i < count($pendientes); $i++){
+                                $idAmigo = $pendientes[$i];
+                                $sql = "SELECT nombre, imagenPerfil FROM usuario WHERE idUsuario = $idAmigo";
+                                $respuesta = mysqli_fetch_array(solicitarDatos($sql));
+                                $imagen = $respuesta[1];
+                                $nombre = $respuesta[0];
+
+                                echo '<li>
+                                    <form action="perfil.php" method="POST">
+                                        <ul class="solicitud">
+                                            <li>
+                                                <img class="amiSol" src="data:image/png;base64,' .  base64_encode($imagen)  . '" alt="Imagen de perfil">
+                                                <label for="nombre" name="nombre">'.$nombre.'</label>
+                                                <input type="text" value="'.$idAmigo.'" name="solicitud" style="visibility: collapse;">
+                                            </li>
+                                            <li><button type="submit" name="aceptar">Aceptar</button></li>
+                                            <li><button type="submit" name="rechazar">Rechazar</button></li>
+                                            <li><button type="submit" name="bann">Bannear</button></li>
+                                        </ul>
+                                    </form>
+                                </li>';
+                            }
+                        }else echo "<p>No hay solicitudes pendientes</p>";
+                    ?>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            </div>
+            </div>
+        </div>
+        </div>
 <!-- -------------------------------------------------------------------------------------------------------------------------------------------------- -->
-        <script> var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), options); </script>
+        <script> var myModal = new bootstrap.Modal(document.getElementById('exampleModal')); </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     </main>
 </body>
@@ -224,19 +287,66 @@
         if($respuesta){
             if($amigoID == $id) echo "<script>window.alert('No puede poner su mismo identificador')</script>";
             else{
-                $sql = "SELECT idUsuario FROM amigos WHERE idUsuario = $id AND amigo = $amigoID";
+                $sql = "SELECT estado FROM amigos WHERE idUsuario = $id AND amigo = $amigoID";
                 $request = mysqli_fetch_array(solicitarDatos($sql));
 
-                if($request) echo "<script>window.alert('Ya ha agregado a el jugador')</script>";
+                if($request[0] == 1) echo "<script>window.alert('Ya ha agregado a el jugador')</script>";
+                elseif($request[0] == 0) echo "<script>window.alert('Ya ha mandado solicitud')</script>";
+                elseif($request[0] == 2) echo "<script>window.alert('No puede mandar solicitud a este usuario')</script>";
                 else{
-                    $sql = "INSERT INTO amigos(idUsuario, amigo) VALUES $id, $amigoID";
-                    guardarDatos($sql);
+                    $sql = "SELECT estado FROM amigos WHERE idUsuario = $amigoID AND amigo = $id";
+                    $respuesta = mysqli_fetch_array(solicitarDatos($sql));
+                    
+                    if($respuesta[0] == 0) echo "<script>window.alert('El usuario ya le mando solicitud')</script>";
+                    else{
+                        $sql = "INSERT INTO amigos(idUsuario, amigo, estado) VALUES ($id, $amigoID, 0)";
+                        guardarDatos($sql);
 
-                    echo "<script>window.alert('Amigo agregado')</script>";
+                        "<script>window.alert('Solicitud enviada')</script>";
+                    }
                 }
             }
         }else{
             echo "<script>window.alert('El identificador no existe')</script>";
         }
+    }
+
+    if(isset($_POST['aceptar'])){
+        $idSol = $_POST['solicitud'];
+        
+        $sql = "UPDATE amigos SET estado = 1 WHERE idUsuario = $idSol AND amigo = $id";
+        guardarDatos($sql);
+
+        $sql = "INSERT INTO amigos(idUsuario, amigo, estado) VALUES ($id, $idSol, 1)";
+        guardarDatos($sql);
+
+        echo "<script>window.alert('Solicitud aceptada')</script>";
+    }
+
+    if(isset($_POST['rechazar'])){
+        $idSol = $_POST['solicitud'];
+
+        $sql = "DELETE FROM amigos WHERE idUsuario = $idSol AND amigo = $id";
+        guardarDatos($sql);
+
+        echo "<script>window.alert('Solicitud rechazada')</script>";
+    }
+
+    if(isset($_POST['bann'])){
+        $idSol = $_POST['solicitud'];
+
+        $sql = "UPDATE amigos SET estado = 2 WHERE idUsuario = $idSol AND amigo = $id";
+        guardarDatos($sql);
+
+        echo "<script>window.alert('El usuario ya no podrá mandarte solicitud')</script>";
+    }
+
+    if(isset($_POST['verPerfil'])){
+        $_SESSION['amigo'][0] = $_POST['idamigo'];
+        $_SESSION['amigo'][1] = $_POST['nombreamigo'];
+
+        echo "<script> 
+        window.location.replace('perfilAmigo.php'); 
+        </script>";
     }
 ?>
